53e18223516ea91bba37ff9ecb64c74bb833f55a
Amrit Bhogal <ambhogal01@gmail.com>
2024-01-16 12:31:41 -0800

    remove gplv3 licene on path utls

--- path-utilities.lua
+++ path-utilities.lua
@@ -1,20 +1,3 @@
--- Copyright (C) 2023 Amrit Bhogal
---
--- This file is part of Portfolio.
---
--- Portfolio is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- Portfolio is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with Portfolio.  If not, see <http://www.gnu.org/licenses/>.
-
 ---@diagnostic disable-next-line: deprecated
 local unpack = unpack or table.unpack
 
